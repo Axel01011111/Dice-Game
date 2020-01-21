715a889c6439445d8d9667e4d03bc14bba487c01
Axel <53453024+Axel01011111@users.noreply.github.com>
2020-01-21 18:45:48 -0300

    added button

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,20 +3,26 @@ let randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
 let randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
 
 //Second, select a random dice
-let rollDice1 = "images/dice"+randomNumber1+".png";
-let rollDice2 = "images/dice"+randomNumber2+".png";
+let rollDice1 = "images/dice" + randomNumber1 + ".png";
+let rollDice2 = "images/dice" + randomNumber2 + ".png";
 
-//Third, set them
-document.querySelectorAll("img")[0].setAttribute("src", rollDice1);
-document.querySelector(".img2").src = rollDice2;
+//Third, press play and set them
+document.querySelector("button").addEventListener("click", rollEmGood);
 
-//Check who won
-  if(randomNumber1 < randomNumber2){
+
+function rollEmGood() {
+  //Set dices
+  document.querySelectorAll("img")[0].setAttribute("src", rollDice1);
+  document.querySelector(".img2").src = rollDice2;
+
+
+  //Check who won
+  if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-  }
-  else if(randomNumber1 > randomNumber2){
+  } else if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
-  }
-  else{
+  } else {
     document.querySelector("h1").innerHTML = "DRAW!";
   }
+
+}
