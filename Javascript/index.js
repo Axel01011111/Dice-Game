4ba6694fc301dc439501a861023f3b615e5eac50
Axel <53453024+Axel01011111@users.noreply.github.com>
2020-01-22 17:22:31 -0300

    Fixing the Button

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,22 +1,19 @@
-//First, the two random numbers
-let randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
-let randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
+//First, the function
+document.querySelector("button").addEventListener("click", function() {
 
-//Second, select a random dice
-let rollDice1 = "images/dice" + randomNumber1 + ".png";
-let rollDice2 = "images/dice" + randomNumber2 + ".png";
+  //Second, setting the numbers
+  let randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
+  let randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
 
-//Third, press play and set them
-document.querySelector("button").addEventListener("click", rollEmGood);
+  //Third, create random dices
+  let rollDice1 = "images/dice" + randomNumber1 + ".png";
+  let rollDice2 = "images/dice" + randomNumber2 + ".png";
 
-
-function rollEmGood() {
-  //Set dices
+  //Fourth, Set dices
   document.querySelectorAll("img")[0].setAttribute("src", rollDice1);
   document.querySelector(".img2").src = rollDice2;
 
-
-  //Check who won
+  //Fifth, Check who won
   if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
   } else if (randomNumber1 > randomNumber2) {
@@ -24,5 +21,4 @@ function rollEmGood() {
   } else {
     document.querySelector("h1").innerHTML = "DRAW!";
   }
-
-}
+});
